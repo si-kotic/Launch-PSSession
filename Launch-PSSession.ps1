171ad79acf076419a1a035ee9585f348d25de2da
Simon Brown <simonb@efstechnology.com>
2018-08-31 11:32:08 +0100

    First Commit

---
+++ Launch-PSSession.ps1
@@ -0,0 +1,33 @@
+Function Launch-PSSession {
+    Param (
+        $ComputerName,
+        [ValidateSet("Black","DarkBlue","DarkGreen","DarkCyan","DarkRed","DarkYellow","Gray","DarkGray")]$BackgroundColour
+    )
+    $colourPairs = [Management.Automation.PSObject]@{
+        Black = "Yellow"
+        DarkBlue = "White"
+        DarkGreen = "White"
+        DarkCyan = "White"
+        DarkRed = "Yellow"
+        DarkYellow = "DarkGreen"
+        Gray = "DarkBlue"
+        DarkGray = "Yellow"
+    }
+    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    If ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        $userLevel = "Administrator"
+    }
+    Else {
+        $userLevel = "User"
+    }
+    # Connect to PSSession
+    New-PSSession $ComputerName | Enter-PSSession
+    # Set the Window Title
+    $host.UI.rawUI.WindowTitle = "[" + $ComputerName.toUpper() + "] " + $userLevel + ": Windows PowerShell"
+    # Set the Background Colour
+    $host.UI.rawUI.BackgroundColor = $BackgroundColour
+    # Set the Foreground Colour
+    $host.UI.rawUI.ForegroundColor = $colourPairs.$BackgroundColour
+    # Clear the screen?
+    Clear-Host
+}
